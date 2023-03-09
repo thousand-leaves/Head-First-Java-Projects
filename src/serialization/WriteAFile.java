fd5686c0d5dc160d15b23599a1af362762bf47e1
thousand-leaves <90856315+thousand-leaves@users.noreply.github.com>
2023-03-09 15:03:42 +0000

    updating with quizcards

---
+++ src/serialization/WriteAFile.java
@@ -0,0 +1,18 @@
+package serialization;
+
+import java.io.*;
+
+public class WriteAFile {
+
+	public static void main(String[] args) {
+		try {
+			FileWriter writer = new FileWriter("Foo.txt");
+			
+			writer.write("hello foo!");
+			
+			writer.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}	
+	}
+}
